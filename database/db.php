057ff56836a7da9a84383bc0d051052db3af0eb8
Roaa Alhakimi <58307502+ro5a@users.noreply.github.com>
2022-03-22 13:33:50 +0300

    create class db

--- database/db.php
+++ database/db.php
@@ -1,9 +1,24 @@
 <?php
-class db{
-    private $localhost;
-    private $password;
-    private $username;
-    
+$dns = "mysql:host=localhost";
+$username = 'root';
+$password = '';
 
-}
-?>
+
+class Database
+{
+  private $conn;
+  private $select_query;
+  
+
+  public function __construct($db_name)
+  {
+    global $dns, $username, $password;
+   
+
+    $this->conn = new PDO($dns, $username, $password, [
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+
+    $this->conn->exec("CREATE DATABASE IF NOT EXISTS `$db_name`; USE `$db_name`");
+  }
